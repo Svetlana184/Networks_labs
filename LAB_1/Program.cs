001db0dae174eb465e4efd2fd1e071f8cd0ae0d2
Svetlana184 <sveta.kgd.39@gmail.com>
2025-09-26 15:22:21 +0200

    couple exp

--- LAB_1/Program.cs
+++ LAB_1/Program.cs
@@ -3,14 +3,16 @@ using LAB_1;
 using LAB_1.Models;
 using static System.Reflection.Metadata.BlobBuilder;
 
-//6 11 16 21 26 31 36 41 46 51 56 61 
+
 using ( SapunovaBooksContext db = new SapunovaBooksContext())
 {
-    //Author author = new Author { NameAuthor = Lorem.Sentence() };
+    //добавление данных
+
+
 
     //6. Выбрать из таблицы Books названия книг и количество страниц
     //(поля Title_book и Pages), а из таблицы Authors выбрать имя соответствующего автора книги(поле Name_ author).
-    var arr = db.Books.Join(db.Authors,
+    var books_with_authors = db.Books.Join(db.Authors,
         b => b.CodeAuthor,
         a => a.CodeAuthor,
         (b, a) => new
@@ -19,31 +21,37 @@ using ( SapunovaBooksContext db = new SapunovaBooksContext())
             Pages = b.Pages,
             NameAuthor = a.NameAuthor
         } );
-    foreach ( var item in arr ) Console.WriteLine($"title: {item.Title} pages: {item.Pages} author: {item.NameAuthor}");
+    Console.WriteLine("Названия и количсетво страниц всех книг:");
+    foreach ( var item in books_with_authors) Console.WriteLine($"title: {item.Title} pages: {item.Pages} author: {item.NameAuthor}");
 
 
     //11. . Выбрать из таблицы Authors фамилии, имена, отчества авторов
     //(поле Name_ author), значения которых начинаются с ‘Иванов’.
-    var arr2 = db.Authors.Where(p => p.NameAuthor.StartsWith("Иванов")).Select(p => p.NameAuthor).ToList();
-    foreach (var item in arr2) Console.WriteLine($"{item}");
+    var ivanov = db.Authors.Where(p => p.NameAuthor.StartsWith("Иванов")).Select(p => p.NameAuthor).ToList();
+    Console.WriteLine("Все Фио авторов, начинающиеся с \"Иванов\":");
+    foreach (var item in ivanov) Console.WriteLine($"{item}");
 
 
     //16. Вывести список названий книг (поле Title_book) и количество
     //страниц(поле Pages) из таблицы Books, у которых объем в страницах укладывается в
     //диапазон 200 – 300(условие по полю Pages).
+    var title_pages = db.Books.Where(p => p.Pages >= 200 && p.Pages <= 300).Select(b => new { Title = b.TitleBook, Pages = b.Pages }).ToList();
+    Console.WriteLine("книги с количеством страниц от 200 до 300");
+    foreach (var item in title_pages) Console.WriteLine($"title: {item.Title}, pages: {item.Pages}");
 
     //21. Вывести список авторов (поле Name_author) из таблицы Authors, которые начинаются на букву ‘К’.
-    var arr3 = db.Authors.Where(p => p.NameAuthor.StartsWith("К")).Select(p => p.NameAuthor).ToList();
-    foreach (var item in arr3) Console.WriteLine($"{item}");
+    var starts_with_k = db.Authors.Where(p => p.NameAuthor.StartsWith("К")).Select(p => p.NameAuthor).ToList();
+    Console.WriteLine("авторы на букву К");
+    foreach (var item in starts_with_k) Console.WriteLine($"{item}");
 
 
-    //26. Вывести список издательств (поле Publish) из таблицы Publishing_house,
+    //26. Вывести список издательств (поле Publish) из таблицы Publishing_house,
     //в которых выпущены книги, названия которых (поле Title_book)
     //начинаются со слова ‘Труды’ и город издания(поле City) – ‘Новосибирск’.
 
 
     //31. Вывести суммарную стоимость партии одноименных книг
-    //(использовать поля Amount и Cost) и название книги (поле Title_book) в каждой поставке.
+    //(использовать поля Amount и Cost) и название книги (поле Title_book) в каждой поставке.
 
 
     //36. Вывести среднюю стоимость (использовать поле Cost) и среднее количество экземпляров
@@ -59,16 +67,35 @@ using ( SapunovaBooksContext db = new SapunovaBooksContext())
 
 
     //Вывести список книг (поле Title_book), у которых количество страниц
-    //(поле Pages) больше среднего количества страниц всех книг в таблице. .
+    //(поле Pages) больше среднего количества страниц всех книг в таблице. 
+    var more_pages = db.Books.Where(p => p.Pages > db.Books.Average(p => p.Pages)).Select(p => p.TitleBook).ToList();
+    Console.WriteLine("Cписок книг , у которых количество страниц больше среднего количества страниц:");
+    foreach (var item in more_pages) Console.WriteLine(item);
 
 
     //56. Вывести список книг (поле Title_book), которые были поставлены
-    //поставщиком ‘ЗАО Квантор’ (условие по полю Name_company). .
+    //поставщиком ‘ЗАО Квантор’ (условие по полю Name_company). 
+    var from_kvantor = db.Books.Join(db.PublishingHouses.Where(p => p.Publish == "ЗАО Квантор"),
+        b => b.CodePublish,
+        p => p.CodePublish,
+        (b, p) =>
+        new
+        {
+            Title = b.TitleBook
+        });
+    Console.WriteLine("Список книг, которые были поставлены ‘ЗАО Квантор'");
+    foreach (var item in from_kvantor) Console.WriteLine(item.Title);
 
 
     //61. Добавить в таблицу Books новую запись, причем вместо ключевого
     //поля поставить код(поле Code_book), автоматически увеличенный на единицу
-    //от максимального кода в таблице, вместо названия книги(поле Title_book) написать ‘Наука.Техника.Инновации’.
+    //от максимального кода в таблице, вместо названия книги(поле Title_book) написать ‘Наука.Техника.Инновации’.
+    Console.WriteLine("создаём новую книгу");
+    Book new_book = new Book { TitleBook = "Наука.Техника.Инновации", CodeAuthor = 1, CodePublish = 1 };
+    db.Books.Add(new_book);
+    db.SaveChanges();
+    var book_check = db.Books.FirstOrDefault(p => p.TitleBook == "Наука.Техника.Инновации");
+    Console.WriteLine(book_check.TitleBook);
 
 
 }
--- LAB_1/SapunovaBooksContext.cs
+++ LAB_1/SapunovaBooksContext.cs
@@ -30,7 +30,7 @@ public partial class SapunovaBooksContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=TEACHERPC;Initial Catalog=Sapunova_Books;User ID=user12;Password=user12;Encrypt=False");
+        => optionsBuilder.UseSqlServer("Data Source=GOBLINSCOMP3;Initial Catalog=Sapunova_Books;User ID=sa;Password=1234;Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
